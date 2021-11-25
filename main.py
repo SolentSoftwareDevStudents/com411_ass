dfd61fce8ad23f9f1821f3872b0e183f79856e8c
IgorGrey <5kanchi95@solent.ac.uk>
2021-11-25 14:52:12 +0000

    asb

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ Note:   any user input/output should be done using the appropriate functions in 
 
 
 # Task 10: Import required modules
-# TODO: Your code here
+import tui
+import process
+import visual
 
 # Task 11: Create an empty list named 'covid_records'.
 # This will be used to store the data read from the source data file.
 # TODO: Your code here
-
+covid_records = []
 
 def run():
     # Task 12: Call the function welcome of the module 'tui'.
     # This will display our welcome message when the program is executed.
     # TODO: Your code here
-
+welcome()
     # Task 13: Load the data.
     # - Use the appropriate function in the module 'tui' to display a message to indicate that the data loading
     # operation has started.
